d20516e6a09ca638a5135f1a7aaa327c235a600f
mikhaillapshin <nskursa@gmail.com>
2025-01-08 12:38:22 +0700

    create conftest.py

--- BaseApp.py
+++ BaseApp.py
@@ -24,4 +24,20 @@ class BasePage:
         return element
 
 
+    def get_element_property(self, locator, property):
+        element = self.find_element(locator)
+        if element:
+            return element.value_of_css_property(property)
+        else:
+            logging.error(f"Property {property} not found in element with locator {locator}")
+            return None
+
+    def go_to_site(self):
+        try:
+            start_browsing = self.driver.get(self.base_url)
+        except:
+            logging.exception("Exception while open site")
+            start_browsing = None
+        return start_browsing
+
 
