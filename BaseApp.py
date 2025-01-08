6450758e4f498c07781e4b17b606a592f7a2d703
mikhaillapshin <nskursa@gmail.com>
2025-01-08 12:07:01 +0700

    add def find_element to BaseApp

--- BaseApp.py
+++ BaseApp.py
@@ -4,3 +4,24 @@ import yaml
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+with open("./testdata.yaml") as f:
+    testdata = yaml.safe_load(f)
+
+
+class BasePage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.base_url = testdata["address"]
+
+
+    def find_element(self, locator, time=10):
+        try:
+            element = WebDriverWait(self.driver, time).until(EC.presence_of_element_located(locator),
+                                                             message=f"Can't find element by locator {locator}")
+        except:
+            logging.exception("Find element exception")
+            element = None
+        return element
+
+
+
